c7bf4ff1e44c860584473c0427c7fe4382dc97f5
Jonathan Huston <jonathan.e.huston@gmail.com>
2022-04-08 01:52:33 -0400

    Applied black.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+
 class CategoryInLine(admin.TabularInline):
     model = Post.categories.through
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    inlines = [CategoryInLine,]
+    inlines = [
+        CategoryInLine,
+    ]
     pass
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    exclude = ('posts',)
-
+    exclude = ("posts",)
--- polling/models.py
+++ polling/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.db import models
 
+
 class Poll(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
